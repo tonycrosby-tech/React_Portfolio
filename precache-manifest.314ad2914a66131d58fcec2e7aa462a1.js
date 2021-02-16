eaa383e977d688c9421f3f0692cd4766af0e632a
tony913 <tonycrosby96@gmail.com>
2021-02-16 14:32:56 -0600

    Updates

---
+++ precache-manifest.314ad2914a66131d58fcec2e7aa462a1.js
@@ -0,0 +1,58 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a1c07a8a3a1cc1dc2d6d772fc520f3a6",
+    "url": "/index.html"
+  },
+  {
+    "revision": "39505a581274a6f8ee63",
+    "url": "/static/css/main.597909b4.chunk.css"
+  },
+  {
+    "revision": "6ccf08673808ed942d11",
+    "url": "/static/js/2.6ac51ad9.chunk.js"
+  },
+  {
+    "revision": "7069b205d702e72b0206de48781de38a",
+    "url": "/static/js/2.6ac51ad9.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "39505a581274a6f8ee63",
+    "url": "/static/js/main.540b090d.chunk.js"
+  },
+  {
+    "revision": "4f2b1704eab49d50853b",
+    "url": "/static/js/runtime-main.19ccc7f6.js"
+  },
+  {
+    "revision": "57e30330e38ff8cb44f0be57daa47686",
+    "url": "/static/media/avatar.57e30330.png"
+  },
+  {
+    "revision": "ec5b08f6d3360062d7adf63bc6af82d3",
+    "url": "/static/media/coding-bkg.ec5b08f6.jpg"
+  },
+  {
+    "revision": "5dd96aaaed8ccee8ef30f2699b3bbb4b",
+    "url": "/static/media/first_website.5dd96aaa.JPG"
+  },
+  {
+    "revision": "500568d5a4ef90ad42a9bd6676b36e21",
+    "url": "/static/media/jsquiz.500568d5.jpg"
+  },
+  {
+    "revision": "ac14011b718b4e89809004ac2c6b1810",
+    "url": "/static/media/password-generator.ac14011b.jpg"
+  },
+  {
+    "revision": "b5fd1622664becbfdb6e302199a131fe",
+    "url": "/static/media/sportzilla-demo.b5fd1622.gif"
+  },
+  {
+    "revision": "02f31d24a2e088469eac9426610898a4",
+    "url": "/static/media/weather-dashboard.02f31d24.jpg"
+  },
+  {
+    "revision": "4f1d12596a47bb3571b0ed4c03adc049",
+    "url": "/static/media/website1.4f1d1259.JPG"
+  }
+]);
