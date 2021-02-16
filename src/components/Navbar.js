9af8a02a399296c3c276f142b682a2dc4b1efc57
tony913 <tonycrosby96@gmail.com>
2021-02-16 12:54:54 -0600

    adding navbar items

---
+++ src/components/Navbar.js
@@ -0,0 +1,58 @@
+import React, { useState } from "react";
+import { Link } from "react-router-dom";
+import Drawer from "@material-ui/core/Drawer";
+import Box from "@material-ui/core/Box";
+import AppBar from "@material-ui/core/AppBar";
+import Toolbar from "@material-ui/core/Toolbar";
+import IconButton from "@material-ui/core/IconButton";
+import List from "@material-ui/core/List";
+import ListItem from "@material-ui/core/ListItem";
+import ListItemIcon from "@material-ui/core/ListItemIcon";
+import ListItemText from "@material-ui/core/ListItemText";
+import Avatar from "@material-ui/core/Avatar";
+import Divider from "@material-ui/core/Divider";
+import Typography from "@material-ui/core/Typography";
+import ArrowBack from "@material-ui/icons/ArrowBack";
+import AssignmentInd from "@material-ui/icons/AssignmentInd";
+import Home from "@material-ui/icons/Home";
+import Apps from "@material-ui/icons/Apps";
+import ContactMail from "@material-ui/icons/ContactMail";
+import { makeStyles } from "@material-ui/core/styles";
+import avatar from "../avatar.png";
+
+import Footer from "../components/Footer";
+
+const useStyles = makeStyles((theme) => ({
+  appbar: {
+    background: "#00008b",
+    margin: 0,
+  },
+  arrow: {
+    color: "whitesmoke",
+  },
+  title: {
+    color: "#d3d3d3",
+  },
+  menuSliderContainer: {
+    width: 250,
+    background: "#d3d3d3",
+    height: "100%",
+    color: "#ad8e6"
+  },
+  avatar: {
+    display: "block",
+    margin: "0.5rem auto",
+    width: theme.spacing(13),
+    height: theme.spacing(13),
+  },
+  listItem: {
+    color: "black",
+  },
+}));
+
+const menuItems = [
+  { listIcon: <Home />, listText: "Home", listPath: "/" },
+  { listIcon: <AssignmentInd />, listText: "Resume", listPath: "/resume" },
+  { listIcon: <Apps />, listText: "Portfolio", listPath: "/portfolio" },
+  { listIcon: <ContactMail />, listText: "Contact", listPath: "/contact" },
+];
