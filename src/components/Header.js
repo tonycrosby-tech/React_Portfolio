8217f78e80a1681786a88e32d1d9c6491fb1de7f
tony913 <tonycrosby96@gmail.com>
2021-02-16 12:53:44 -0600

    adding header

---
+++ src/components/Header.js
@@ -0,0 +1,32 @@
+import React from "react";
+import Typography from "@material-ui/core/Typography";
+import Avatar from "@material-ui/core/Avatar";
+import Grid from "@material-ui/core/Grid";
+import Box from "@material-ui/core/Box";
+import Typed from "react-typed";
+import { makeStyles } from "@material-ui/core/styles";
+import avatar from "../avatar.png";
+
+const useStyles = makeStyles((theme) => ({
+  avatar: {
+    width: theme.spacing(15),
+    height: theme.spacing(15),
+    margin: theme.spacing(1),
+  },
+  title: {
+    color: "whitesmoke",
+  },
+  subtitle: {
+    color: "#add8e6",
+    textTransform: "uppercase",
+  },
+  typedContainer: {
+    position: "absolute",
+    top: "50%",
+    left: "50%",
+    transform: "translate(-50%,-50%)",
+    width: "100vw",
+    textAlign: "center",
+    zIndex: 1,
+  },
+}));
